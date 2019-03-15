a7d6e513567b1c0330b5af36ef7bc0e7347be93b
Robert Miller <robert.miller@curavihealth.com>
2019-03-15 11:40:42 -0400

    Added express server

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,5 @@ var hub = new HubRegistry(['gulp/tasks/*.js']);
 gulp.registry(hub);
 
 gulp.task('dev', gulp.series('clean', 'build', 'copy', 'devWatch'));
+gulp.task('build', gulp.series('clean', 'build', 'copy'));
 gulp.task('default', gulp.series('dev'));
--- server/app.js
+++ server/app.js
@@ -9,6 +9,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 module.exports = app;
